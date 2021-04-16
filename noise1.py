2acea768ba0499ca51fe1cd435fcbaf09d83e981
amp212 <amp212@cam.ac.uk>
2021-04-16 21:07:28 +0100

    added simple perlin noise & test

--- noise1.py
+++ noise1.py
@@ -39,6 +39,7 @@ class DataSet:
         generates random data using several methods
         method:
         Gaussian: X ~ N(mu, sigma**2)
+        Perlin: chunks
         """
 
         #sample from Gaussian distribution
@@ -46,6 +47,41 @@ class DataSet:
             if self.name == "":
                 self.name = "Gaussian ({:.1f},{:.1f})".format(kwargs["mu"], kwargs["sigma"])
             self.data = np.random.normal(loc=kwargs["mu"], scale=kwargs["sigma"], size=self.axis_shape)
+
+        #generate Perlin noise
+        elif method == "Perlin":
+            if self.name == "":
+                self.name = "Perlin"
+
+            #generate random vectors for gradients of each node
+            chunk_size = int(round(self.axis_shape[0]/kwargs["chunks"]))
+            node_locations = np.linspace(self.axis[0], self.axis[-1], chunk_size)
+            node_gradients = np.random.uniform(low=-1, high=1, size=np.shape(node_locations))
+
+            self.data = []
+            #iterate over axis
+            for i, point in enumerate(self.axis):
+                #find nearest nodes, locations and gradients
+                left_node_index = np.argmin(np.abs(node_locations-point))
+                right_node_index = left_node_index + 1
+                if right_node_index == np.shape(node_locations)[0]:
+                    right_node_index = left_node_index
+                left_node_location = node_locations[left_node_index]
+                right_node_location = node_locations[right_node_index]
+                left_node_gradients = node_gradients[left_node_index]
+                right_node_gradients = node_gradients[right_node_index]
+
+                #calculate distance vectors
+                left_node_distance = point-left_node_location
+                right_node_distance = right_node_location-point
+
+                #calculate dot product with gradient vector
+                left_node_value = left_node_distance*left_node_gradients
+                right_node_value = right_node_distance*right_node_gradients
+
+                #linearly interpolate for value
+                value = np.interp(point, [left_node_location,right_node_location], [left_node_value,right_node_value])
+                self.data.append(value)
 
     def calculate_fft(self):
         """calculates the fft of the data"""
@@ -64,7 +100,7 @@ class DataSet:
         plt.plot(self.axis, self.data, self.color)
         plt.show()
 
-def test_1(plots):
+def test_gaussian(plots):
     fig, axs = plt.subplots(plots)
 
     for i in range(0,plots):
@@ -77,5 +113,13 @@ def test_1(plots):
     plt.legend()
     plt.show()
 
+def test_perlin():
+    test_set = DataSet(name = "Perlin Test", axis_start=0, axis_stop=1000, axis_division = 0.5)
+    test_set.generate_random(method="Perlin", chunks=30)
+
+    fig, ax = plt.subplots()
+    ax.plot(test_set.axis, test_set.data, color=test_set.color, label=test_set.name)
+    plt.show()
+
 if __name__ == "__main__":
-    test_1(5)
+    test_perlin()
